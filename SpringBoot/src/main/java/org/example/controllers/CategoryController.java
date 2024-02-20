0a2cbcc798c8057e6381ae47382016e4021ca442
yrakhwas <yrakhwas@gmail.com>
2024-02-20 17:44:47 +0200

    Create post/put method in Category Controller

--- SpringBoot/src/main/java/org/example/controllers/CategoryController.java
+++ SpringBoot/src/main/java/org/example/controllers/CategoryController.java
@@ -8,6 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 import java.util.List;
 
@@ -21,5 +26,23 @@ public class CategoryController {
     public ResponseEntity<List<CategoryEntity>> index() {
         List<CategoryEntity> list = categoryRepository.findAll();
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<CategoryEntity> addCategory(@RequestBody CategoryEntity category){
+        CategoryEntity addedCategory = categoryRepository.save(category);
+        return new ResponseEntity<>(addedCategory, HttpStatus.CREATED);
+    }
+    @PutMapping
+    public ResponseEntity<CategoryEntity> editCategory(@PathVariable int categoryId, @RequestBody CategoryEntity updatedCategory){
+        if(categoryRepository.existsById(categoryId))
+        {
+            updatedCategory.setId(categoryId);
+            CategoryEntity editedCategory = categoryRepository.save(updatedCategory);
+            return new ResponseEntity<>(editedCategory, HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
